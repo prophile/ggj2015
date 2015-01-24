05b9da135a3cc15f960754043a1f0348d5ed03f4
Alistair Lynn <arplynn@gmail.com>
2015-01-24 14:07:28 +0000

    Add some bees

--- core/src/uk/co/alynn/games/suchrobot/NodeSet.java
+++ core/src/uk/co/alynn/games/suchrobot/NodeSet.java
@@ -7,8 +7,34 @@ import java.util.List;
 import java.util.Map;
 
 public class NodeSet implements Iterable<PathNode> {
+    private static final class RoutingKey {
+        public final String sourceNode;
+        public final String destNode;
+        
+        public RoutingKey(String src, String dst) {
+            sourceNode = src;
+            destNode = dst;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this)
+                return true;
+            if (!(obj instanceof RoutingKey))
+                return false;
+            RoutingKey o = (RoutingKey)obj;
+            return o.sourceNode.equals(sourceNode) && o.destNode.equals(destNode);
+        }
+        
+        @Override
+        public int hashCode() {
+            return (903*sourceNode.hashCode()) + destNode.hashCode();
+        }
+    }
+    
     private List<PathNode> nodes = new ArrayList<PathNode>();
     private Map<String, List<PathNode>> directConnections = new HashMap<String, List<PathNode>>();
+    private Map<RoutingKey, PathNode> nextHops = null;
     
     public NodeSet() {
         
@@ -26,10 +52,16 @@ public class NodeSet implements Iterable<PathNode> {
     }
     
     public void compile() {
-        System.err.println("compiled node set");
+        System.err.println("computing routing tables");
+        nextHops = new HashMap<RoutingKey, PathNode>();
         for (PathNode node : this) {
             System.err.println("- " + node);
+            runDijkstra(node);
         }
+    }
+    
+    private void runDijkstra(PathNode node) {
+        // dijkstra's algorithm!
     }
     
     public PathNode lookup(String name) {
