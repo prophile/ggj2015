a5a37984f04362fc262c8e6aa64ca2911cf0c86b
Alistair Lynn <arplynn@gmail.com>
2015-03-27 19:32:28 +0000

    Initial cut at an iOS build

---
+++ ios/src/uk/co/alynn/games/suchrobot/IOSLauncher.java
@@ -0,0 +1,27 @@
+package uk.co.alynn.games.suchrobot;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.useCompass = false;
+        config.useAccelerometer = false;
+        config.orientationLandscape = false;
+        config.orientationPortrait = true;
+        config.preventScreenDimming = true;
+
+        return new IOSApplication(new RobotGame(), config);
+    }
+
+    public static void main(String[] args) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(args, null, IOSLauncher.class);
+        pool.close();
+    }
+}
