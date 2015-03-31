4220a2a82726aa59eccbfb4986d168bd67243857
Alistair Lynn <arplynn@gmail.com>
2015-03-31 21:06:07 +0100

    Mark NodeSet as final

--- core/src/uk/co/alynn/games/suchrobot/NodeSet.java
+++ core/src/uk/co/alynn/games/suchrobot/NodeSet.java
@@ -15,7 +15,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
-public class NodeSet implements Iterable<PathNode> {
+public final class NodeSet implements Iterable<PathNode> {
     private static final class RoutingKey {
         public final String sourceNode;
         public final String destNode;
